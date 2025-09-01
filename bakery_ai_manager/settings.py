07270c254595e40a9086afe584a0760f9fbebc67
bargah1 <bargah916@gmail.com>
2025-09-01 20:56:34 +0530

    Update settings.py

--- bakery_ai_manager/settings.py
+++ bakery_ai_manager/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f7m!gghd!_h$^2fd=4%6@x(!)%y0@k0x2g*z5wzi(+w5ig^-5p'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
