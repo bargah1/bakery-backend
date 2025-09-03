064fd2fe96bed6d09c419bf3657fbe09a8c873a6
bargah1 <bargah916@gmail.com>
2025-09-03 11:59:33 +0530

    Update settings.py

--- bakery_ai_manager/settings.py
+++ bakery_ai_manager/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['bakery-backend-5qkn.onrender.com',
+'web-production-b7e2a.up.railway.app', 
+    'localhost',
+    '127.0.0.1',]
 
 # Application definition
 
